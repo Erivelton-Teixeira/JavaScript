02beac30e767e9195b40d823604eb8887e5aca4a
“erivelton-teixeira” <erivelton.tb@hotmail.com>
2026-04-08 22:23:00 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,47 @@
+/**
+ * Estudo das Variaveis
+ * @author Erivelton Teixeira
+ */
+
+console.clear ()
+console.log ("Estudo das variáveis")
+console.log("")
+console.log("Varial do tipo String")
+// a linha abaixo cria uma variável
+let nome = "Erivelton Teixeira"
+console.log(nome)
+//a linha abaixo mosta um tipo de variavel
+console.log(typeof(nome))
+console.log("")
+console.log("Variavel do tipo number")
+let idade = 27
+console.log(idade)
+console.log(typeof(idade))
+console.log("")
+let peso = 70
+console.log(peso)
+console.log(typeof(peso))
+console.log("")
+let altura = 1.75
+console.log(altura)
+console.log(typeof(altura))
+console.log("")
+console.log("Variavel do tipo boolean")
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+
+console.log("")
+console.log("----------------------------")
+console.log("-      Ficha do aluno      -")
+console.log("----------------------------")
+// a alinha abaixo concatena (união) um texto com o 
+//conteudo da variavel
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`peso : ${peso}Kg`)
+console.log(`altura: ${altura}`)
+console.log(`vip : ${vip}`)
+console.log("----------------------------")
+
